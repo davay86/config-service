9d03d9b3d5c8316a5350eaa0b72c05a8c04d57f9
SleepingTalent <jaybono30@hotmail.com>
2017-08-23 10:32:07 +0100

    renamed from config-server to config-api

---
+++ configuration-api-parent/configuration-api/src/main/resources/start-config-api-dev.bat
@@ -0,0 +1,4 @@
+MODE 1000,1000
+
+start "config-server-1" java -jar @executable.server.jar.name@.jar --server.port=1111 --spring.profiles.active=dev
+start "config-server-2" java -jar @executable.server.jar.name@.jar --server.port=1112 --spring.profiles.active=dev
