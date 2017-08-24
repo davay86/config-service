4361fbf30a25d80192da873b7c6cd95c5efbf38d
SleepingTalent <jaybono30@hotmail.com>
2017-08-24 09:14:10 +0100

    added test/ci properties

--- configuration-api-parent/configuration-api/src/main/resources/start-config-api-qa.bat
+++
@@ -1,4 +0,0 @@
-MODE 1000,1000
-
-start "config-server-1" java -jar @executable.server.jar.name@.jar --server.port=1111 --spring.profiles.active=qa
-start "config-server-2" java -jar @executable.server.jar.name@.jar --server.port=1112 --spring.profiles.active=qa
