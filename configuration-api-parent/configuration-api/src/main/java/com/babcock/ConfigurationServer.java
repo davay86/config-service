ad22b7179ad6bc422fd0facd645efdf2103b0831
davay86 <davay86@gmail.com>
2017-09-13 13:26:58 +0100

    VB-2 playing about with commit messages

--- configuration-api-parent/configuration-api/src/main/java/com/babcock/ConfigurationServer.java
+++ configuration-api-parent/configuration-api/src/main/java/com/babcock/ConfigurationServer.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigurationServer {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationServer.class, args);
 	}
